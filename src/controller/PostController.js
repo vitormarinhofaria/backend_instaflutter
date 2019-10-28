21b01b0a5282bf57250f9fdbe79ef4fa00b7200a
Vitor Marinho <vitormarinhofaria@gmail.com>
2019-10-28 10:00:23 -0300

    New listFeed method

--- src/controller/PostController.js
+++ src/controller/PostController.js
@@ -1,5 +1,6 @@
 const Post = require('../models/Post');
 const User = require('../models/Users');
+const Feed = require('../models/Feed');
 
 module.exports = {
   async store(req, res){
@@ -16,9 +17,13 @@ module.exports = {
     if(user){
       user.posts.push(post._id);
       await user.save();
+      for(i = 0; i < user.followers.length; i++){
+        let follower = await User.findById(user.followers[i]);
+        follower.feed.push(post._id);
+        follower.save();
+      }
     }
-    
-    console.log(user);
+    console.log(post);
     return res.json(post);
   },
 
@@ -26,17 +31,51 @@ module.exports = {
     const userId = req.userId;
 
     const loggedUser = await User.findById(userId);
+    const options = {
+      select: 'posts',
+      page: 1,
+      limit: 10,
+      populate: 'posts', sort: {createdAt: -1}, paginate: {page: 1, limit: 10},
+    }
 
     let posts = await User.find({_id: loggedUser.following}, 'posts').populate('posts');
-
+    //let posts = await User.paginate({}, options);
     let response = [];
 
-    for(i = 0; i < posts.length; i++){
-      for(a = 0; a < posts[i].posts.length; a++){
-        response.push(posts[i].posts[a]);
+    for(i = 0; i < posts.docs.length; i++){
+      for(a = 0; a < posts.docs[i].posts.length; a++){
+        response.push(posts.docs[i].posts[a]);
       }
     }
 
     return res.json(response);
+  },
+  async listFeed(req, res){
+    const userId = req.userId;
+
+    const options = {
+      select: 'feed',
+      page: 1, limit: 10,
+      populate: {path: 'feed', options: {
+        sort: {createdAt: -1},
+        offset: 5,
+        limit: 10
+      }}, 
+      //sort:{createdAt: -1},
+    }
+
+    //const feed = await User.paginate({_id: userId}, options);
+    let perPage = 10;
+    let page = Math.max(0, req.query.page)
+    const feed = await User.findById(userId, 'feed').populate([{
+      path: 'feed',
+      options: {
+        sort: {createdAt: -1},
+        skip: (perPage * page),
+        limit: perPage,
+      }
+    }])
+
+    return res.json(feed);
   }
 };
