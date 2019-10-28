70263ae97a9b5514797b03c50a0bb342ca7afda2
Vitor Marinho <vitormarinhofaria@gmail.com>
2019-10-28 09:59:47 -0300

    New feed field in User Model

--- src/models/Users.js
+++ src/models/Users.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate-v2');
 
 const UserSchema = new mongoose.Schema ({
   name: {
@@ -29,7 +30,11 @@ const UserSchema = new mongoose.Schema ({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Post',
   }],
+  feed:[{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post',
+  }],
 },{timestamps: true}
 );
-
+UserSchema.plugin(mongoosePaginate);
 module.exports = mongoose.model ('User', UserSchema);
