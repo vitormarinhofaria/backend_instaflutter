3b79c2a29378dcb2f211d236225994a555c1d664
Vitor Marinho <vitormarinhofaria@gmail.com>
2019-10-30 14:05:42 -0300

    Remove old method

--- src/controller/PostController.js
+++ src/controller/PostController.js
@@ -27,48 +27,18 @@ module.exports = {
     return res.json(post);
   },
 
-  async listFollowing(req, res){
-    const userId = req.userId;
-
-    const loggedUser = await User.findById(userId);
-    const options = {
-      select: 'posts',
-      page: 1,
-      limit: 10,
-      populate: 'posts', sort: {createdAt: -1}, paginate: {page: 1, limit: 10},
-    }
-
-    let posts = await User.find({_id: loggedUser.following}, 'posts').populate('posts');
-    //let posts = await User.paginate({}, options);
-    let response = [];
-
-    for(i = 0; i < posts.docs.length; i++){
-      for(a = 0; a < posts.docs[i].posts.length; a++){
-        response.push(posts.docs[i].posts[a]);
-      }
-    }
-
-    return res.json(response);
-  },
   async listFeed(req, res){
     const userId = req.userId;
 
-    const options = {
-      select: 'feed',
-      page: 1, limit: 10,
-      populate: {path: 'feed', options: {
-        sort: {createdAt: -1},
-        offset: 5,
-        limit: 10
-      }}, 
-      //sort:{createdAt: -1},
-    }
-
-    //const feed = await User.paginate({_id: userId}, options);
     let perPage = 10;
-    let page = Math.max(0, req.query.page)
+    let page = Math.max(0, req.query.page);
+
     const feed = await User.findById(userId, 'feed').populate([{
       path: 'feed',
+      populate: {
+        path: 'postedBy',
+        select: 'username',
+      },
       options: {
         sort: {createdAt: -1},
         skip: (perPage * page),
@@ -76,6 +46,6 @@ module.exports = {
       }
     }])
 
-    return res.json(feed);
+    return res.json(feed.feed);
   }
 };
